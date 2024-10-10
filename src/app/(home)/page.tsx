bf7e3876c0a28f2ee48f693bf3620e1fb16e5039
modrako <olik.cvik@gmail.com>
2024-10-10 09:50:14 +0200

    authp1

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,5 @@
 // src/app/page.tsx
 
-import BottomNavigation from '@mui/material/BottomNavigation';
-import NavBar from "@/components/NavBar"
 import Typography from '@mui/material/Typography';
 
 export const metadata = {title: " Domov | Miligram"}
--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,13 +1,12 @@
 // src/pages/_app.tsx
 import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app';
-import NavBar from 'src/components/NavBar.tsx';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <CssBaseline /> {/* This provides a consistent baseline for styling */}
-      <NavBar />
       <Component {...pageProps} /> {/* This renders the current page */}
     </>
   );
